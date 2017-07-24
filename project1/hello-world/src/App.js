2596571089facdcb8aca7409db9776639b448cf2
George Nguyen <g.nguyen92@yahoo.com>
2017-07-24 13:41:49 -0700

    hello-world: Move Header and Intro into separate files.  Then import into App.js for more modularity

--- project1/hello-world/src/App.js
+++ project1/hello-world/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Intro from './Intro';
 
 
 
@@ -15,28 +17,6 @@ class App extends Component {
   }
 }
 
-class Header extends Component {
-  render(){
-    return(
-      <div>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-      </div>
-    );
-  }
-}
 
-class Intro extends Component {
-  render(){
-    return(
-      <div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      </div>
-    );
-  }
-}
+
 export default App;

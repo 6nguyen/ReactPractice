7c64f2e48260f2ec2f02144e6c89795b7f51a71c
George Nguyen <g.nguyen92@yahoo.com>
2017-07-25 12:21:02 -0700

    blog/app: Replace all 'class' keywords to 'className'

--- full-scale-apps/blog/src/App.js
+++ full-scale-apps/blog/src/App.js
@@ -7,20 +7,20 @@ class App extends Component {
     return (
       <div>
       <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
-            <div class="container-fluid">
+        <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
+            <div className="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header page-scroll">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        Menu <i class="fa fa-bars"></i>
+                <div className="navbar-header page-scroll">
+                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span className="sr-only">Toggle navigation</span>
+                        Menu <i className="fa fa-bars"></i>
                     </button>
-                    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+                    <a className="navbar-brand" href="index.html">Start Bootstrap</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right">
                         <li>
                             <a href="index.html">Home</a>
                         </li>
@@ -42,14 +42,14 @@ class App extends Component {
 
         <!-- Page Header -->
         <!-- Set your background image for this header on the line below. -->
-        <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                        <div class="site-heading">
+        <header className="intro-header" style="background-image: url('img/home-bg.jpg')">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <div className="site-heading">
                             <h1>Clean Blog</h1>
-                            <hr class="small">
-                            <span class="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                            <hr className="small">
+                            <span className="subheading">A Clean Blog Theme by Start Bootstrap</span>
                         </div>
                     </div>
                 </div>
@@ -57,57 +57,57 @@ class App extends Component {
         </header>
 
         <!-- Main Content -->
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="post-preview">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div className="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">
+                            <h2 className="post-title">
                                 Man must explore, and this is exploration at its greatest
                             </h2>
-                            <h3 class="post-subtitle">
+                            <h3 className="post-subtitle">
                                 Problems look mighty small from 150 miles up
                             </h3>
                         </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
+                        <p className="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
                     </div>
                     <hr>
-                    <div class="post-preview">
+                    <div className="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">
+                            <h2 className="post-title">
                                 I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
                             </h2>
                         </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 18, 2014</p>
+                        <p className="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 18, 2014</p>
                     </div>
                     <hr>
-                    <div class="post-preview">
+                    <div className="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">
+                            <h2 className="post-title">
                                 Science has not yet mastered prophecy
                             </h2>
-                            <h3 class="post-subtitle">
+                            <h3 className="post-subtitle">
                                 We predict too much for the next year and yet far too little for the next ten.
                             </h3>
                         </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on August 24, 2014</p>
+                        <p className="post-meta">Posted by <a href="#">Start Bootstrap</a> on August 24, 2014</p>
                     </div>
                     <hr>
-                    <div class="post-preview">
+                    <div className="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">
+                            <h2 className="post-title">
                                 Failure is not an option
                             </h2>
-                            <h3 class="post-subtitle">
+                            <h3 className="post-subtitle">
                                 Many say exploration is part of our destiny, but it’s actually our duty to future generations.
                             </h3>
                         </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on July 8, 2014</p>
+                        <p className="post-meta">Posted by <a href="#">Start Bootstrap</a> on July 8, 2014</p>
                     </div>
                     <hr>
                     <!-- Pager -->
-                    <ul class="pager">
-                        <li class="next">
+                    <ul className="pager">
+                        <li className="next">
                             <a href="#">Older Posts &rarr;</a>
                         </li>
                     </ul>
@@ -119,36 +119,36 @@ class App extends Component {
 
         <!-- Footer -->
         <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                        <ul class="list-inline text-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <ul className="list-inline text-center">
                             <li>
                                 <a href="#">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fa fa-circle fa-stack-2x"></i>
-                                        <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                    <span className="fa-stack fa-lg">
+                                        <i className="fa fa-circle fa-stack-2x"></i>
+                                        <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
                                     </span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fa fa-circle fa-stack-2x"></i>
-                                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                    <span className="fa-stack fa-lg">
+                                        <i className="fa fa-circle fa-stack-2x"></i>
+                                        <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
                                     </span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fa fa-circle fa-stack-2x"></i>
-                                        <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                    <span className="fa-stack fa-lg">
+                                        <i className="fa fa-circle fa-stack-2x"></i>
+                                        <i className="fa fa-github fa-stack-1x fa-inverse"></i>
                                     </span>
                                 </a>
                             </li>
                         </ul>
-                        <p class="copyright text-muted">Copyright &copy; Your Website 2016</p>
+                        <p className="copyright text-muted">Copyright &copy; Your Website 2016</p>
                     </div>
                 </div>
             </div>

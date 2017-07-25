c9ab683e085f9a8a5780fc555d9908b8dda56a26
George Nguyen <g.nguyen92@yahoo.com>
2017-07-25 14:01:30 -0700

    Test: commit changes from branch

--- full-scale-apps/blog/src/App.js
+++ full-scale-apps/blog/src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
         {/* Set your background image for this header on the line below. */}
         <header className="intro-header">
             <div className="container">
-                <div className="row">
+                <div className="row"> 
                     <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                         <div className="site-heading">
                             <h1>Clean Blog</h1>

ba15285db2ddcca44d8ba76ef3e075cee50b52b6
George Nguyen <g.nguyen92@yahoo.com>
2017-07-24 18:02:10 -0700

    project5: Add bare minimum html and jsx into app.js

--- project5/basic/src/app.js
+++ project5/basic/src/app.js
@@ -1,1 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Creating my React App from scratch!</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
